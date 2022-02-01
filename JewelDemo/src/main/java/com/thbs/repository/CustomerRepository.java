935956c31a7ab16256b791807bcf8874dbc6b351
Shivakumar.R.Hombal <58224595+Shivakumar-coder@users.noreply.github.com>
2022-02-01 14:11:40 +0530

    Delete JewelDemo directory

--- JewelDemo/src/main/java/com/thbs/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thbs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thbs.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,String> {
-
-}
--- JewelDemo/src/main/java/com/thbs/repository/ServiceRepository.java
+++
@@ -1,5 +0,0 @@
-package com.thbs.repository;
-
-public interface ServiceRepository {
-
-}
